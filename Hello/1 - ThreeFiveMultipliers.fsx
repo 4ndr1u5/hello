2fce1b231f8f3a0d152cabb77c5d349db80fee3c
4ndr1u5 <andrius.juskevicius@yahoo.com>
2015-11-24 15:35:41 +0200

    inital commit

---
+++ Hello/1 - ThreeFiveMultipliers.fsx
@@ -0,0 +1,14 @@
+﻿//If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+//Find the sum of all the multiples of 3 or 5 below 1000.
+
+let numbers = seq { 0 .. 1000 }
+let filter a numbers = numbers 
+                    |> Seq.filter (fun x -> x%a <> 0)
+
+let result = Array.sum (numbers
+            |> (filter 3)
+            |> (filter 5)
+            |> Seq.toArray)
+
+printfn "%d" result
+
---
+++ Hello/Program.fs
@@ -0,0 +1,27 @@
+﻿// Learn more about F# at http://fsharp.net
+// See the 'F# Tutorial' project for more help.
+open System
+
+
+[<EntryPoint>]
+let main argv = 
+    let name = Console.ReadLine();
+    printfn "Hello %s" name
+
+
+
+    let numbers = seq { 0 .. 1000 }
+    let filter a numbers = numbers 
+                        |> Seq.filter (fun x -> x%a <> 0)
+
+    let result = Array.sum (numbers
+                |> (filter 3)
+                |> (filter 5)
+                |> Seq.toArray)
+    printfn "Hello %A" result
+
+
+    Console.ReadLine(); 
+    0 // return an integer exit code
+
+    
